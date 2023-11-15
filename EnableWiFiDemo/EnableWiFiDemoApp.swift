e38b8d02f7603834f83b4db019b961e28a0d17ca
Inhwan Wee <10ezenfinani@gmail.com>
2023-11-15 16:50:32 +0900

    copy from OpenGoPro

---
+++ EnableWiFiDemo/EnableWiFiDemoApp.swift
@@ -0,0 +1,18 @@
+/* EnableWiFiDemoApp.swift/Open GoPro, Version 2.0 (C) Copyright 2021 GoPro, Inc. (http://gopro.com/OpenGoPro). */
+/* This copyright was auto-generated on Wed, Sep  1, 2021  5:06:09 PM */
+
+//
+//  EnableWiFiDemoApp.swift
+//  EnableWiFiDemo
+//
+
+import SwiftUI
+
+@main
+struct EnableWiFiDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            CameraSelectionView()
+        }
+    }
+}
---
+++ EnableWiFiDemo/Helpers/Atomic.swift
@@ -0,0 +1,37 @@
+/* Atomic.swift/Open GoPro, Version 2.0 (C) Copyright 2021 GoPro, Inc. (http://gopro.com/OpenGoPro). */
+/* This copyright was auto-generated on Wed, Sep  1, 2021  5:06:09 PM */
+
+//
+//  Atomic.swift
+//  EnableWiFiDemo
+//
+
+import Foundation
+
+@propertyWrapper
+struct Atomic<Value> {
+
+    private var value: Value
+    private let lock = NSLock()
+
+    init(wrappedValue value: Value) {
+        self.value = value
+    }
+
+    var wrappedValue: Value {
+      get { return load() }
+      set { store(newValue: newValue) }
+    }
+
+    func load() -> Value {
+        lock.lock()
+        defer { lock.unlock() }
+        return value
+    }
+
+    mutating func store(newValue: Value) {
+        lock.lock()
+        defer { lock.unlock() }
+        value = newValue
+    }
+}
