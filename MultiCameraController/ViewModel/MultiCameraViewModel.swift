7fa2305feb6fa7b9bf81915b1c718066f01e38a1
Inhwan Wee <10ezenfinani@gmail.com>
2024-03-06 21:19:44 +0900

    split MultiCameraView

--- MultiCameraController/ViewModel/MultiCameraViewModel.swift
+++ MultiCameraController/ViewModel/MultiCameraViewModel.swift
@@ -23,7 +23,23 @@ class MultiCameraViewModel: ObservableObject {
       return temp
     }
 
+  @Published var showSettingView = false
+  @Published var showCameraView = false
+
   @Published var cameraConnectionInfoListEditable = false
+  @Published var targetCameraConnectionInfo = CameraConnectionInfo(camera: GoPro(serialNumber: ""))
+  @Published var newCameraSerialNumber: String = ""
+  @Published var downloadMediaUrl: String = ""
+  @Published var downloadProgress: Double = 0.0
+
+  @Published var showCameraToast = false
+  @Published var showShutterOnToast = false
+  @Published var showShutterOffToast = false
+  @Published var showDownloadMediaToast = false
+  @Published var showRemoveMediaToast = false
+  @Published var showRefreshCameraListToast = false
+  @Published var showCameraConnectedToast = false
+  @Published var showCameraEmptyToast = false
 
   @Published var showPreset1Toast = false
   @Published var showVideoResolutionToast = false
@@ -39,6 +55,201 @@ class MultiCameraViewModel: ObservableObject {
 
   func getConnectedCameraList() -> [CameraConnectionInfo] {
     self.cameraConnectionInfoList.filter { $0.isConnected == true }
+  }
+
+  func getCreationTimestamp(completion: @escaping (Int) -> Void) {
+    var creationTimestamp = 2_147_483_647
+    for cameraConnectionInfo in self.getConnectedCameraList() {
+      cameraConnectionInfo.camera.requestUsbMediaList { _, latestCreationTimestamp, error in
+        if error != nil {
+          os_log("Error: %@", type: .error, error?.localizedDescription ?? "")
+          return
+        }
+        creationTimestamp = min(creationTimestamp, latestCreationTimestamp)
+
+        if cameraConnectionInfo == self.cameraConnectionInfoList.last {
+          completion(creationTimestamp)
+        }
+      }
+    }
+  }
+
+  func registerCamera(newCameraSerialNumber: String) {
+    if self.newCameraSerialNumber.count == 3, self.newCameraSerialNumber.isInt(),
+       !self.cameraConnectionInfoList.contains(where: { $0.camera.serialNumber == newCameraSerialNumber })
+    {
+      os_log("Add GoPro %@", type: .info, self.newCameraSerialNumber)
+      self.goProSerialNumberList.append(self.newCameraSerialNumber)
+      self.cameraConnectionInfoList
+        .append(CameraConnectionInfo(camera: GoPro(serialNumber: self.newCameraSerialNumber)))
+
+      let index = self.cameraConnectionInfoList.count - 1
+      os_log(
+        "Enable Wired USB Control: GoPro %@",
+        type: .info,
+        self.cameraConnectionInfoList[index].camera.serialNumber
+      )
+      self.cameraConnectionInfoList[index].camera.requestUsbCameraInfo { _, error in
+        if error != nil {
+          os_log("Error: %@", type: .error, error?.localizedDescription ?? "")
+          if index >= self.cameraConnectionInfoList.count {
+            return
+          }
+          self.cameraConnectionInfoList[index].isConnected = false
+          return
+        }
+
+        if index >= self.cameraConnectionInfoList.count {
+          return
+        }
+        self.cameraConnectionInfoList[index].isConnected = true
+        self.cameraConnectionInfoList[index].camera
+          .requestUsbCommand(command: .enableWiredUsbControl) { error in
+            if error != nil {
+              os_log("Error: %@", type: .error, error?.localizedDescription ?? "")
+              return
+            }
+          }
+      }
+    } else {
+      os_log("%@ is not a serial number (3 digits)", type: .error, self.newCameraSerialNumber)
+    }
+  }
+
+  func deleteCameraItem(at offsets: IndexSet) {
+    os_log(
+      "Remove GoPro %@",
+      type: .info,
+      self.cameraConnectionInfoList[offsets[offsets.startIndex]].camera.serialNumber
+    )
+    self.goProSerialNumberList.remove(atOffsets: offsets)
+    self.cameraConnectionInfoList.remove(atOffsets: offsets)
+  }
+
+  func connectCameraItem(index: Int, showToast: Bool = false) {
+    os_log("Connect GoPro %@", type: .info, self.cameraConnectionInfoList[index].camera.serialNumber)
+    self.cameraConnectionInfoList[index].camera.requestUsbCameraInfo { _, error in
+      if error != nil {
+        os_log("Error: %@", type: .error, error?.localizedDescription ?? "")
+        self.cameraConnectionInfoList[index].isConnected = false
+        if showToast {
+          self.showCameraEmptyToast.toggle()
+        }
+        return
+      }
+      self.cameraConnectionInfoList[index].isConnected = true
+      if showToast {
+        self.showCameraConnectedToast.toggle()
+      }
+
+      os_log(
+        "Enable Wired USB Control: GoPro %@",
+        type: .info,
+        self.cameraConnectionInfoList[index].camera.serialNumber
+      )
+      self.cameraConnectionInfoList[index].camera.requestUsbCommand(command: .enableWiredUsbControl) { error in
+        if error != nil {
+          os_log("Error: %@", type: .error, error?.localizedDescription ?? "")
+          return
+        }
+      }
+    }
+  }
+
+  func startShootingAll() {
+    os_log("Shutter On All", type: .info)
+    for cameraConnectionInfo in self.getConnectedCameraList() {
+      cameraConnectionInfo.camera.requestUsbCommand(command: .shutterOn) { error in
+        if error != nil {
+          os_log("Error: %@", type: .error, error?.localizedDescription ?? "")
+          return
+        }
+      }
+    }
+    self.showShutterOnToast.toggle()
+  }
+
+  func stopShootingAll() {
+    os_log("Shutter Off All", type: .info)
+    for cameraConnectionInfo in self.getConnectedCameraList() {
+      cameraConnectionInfo.camera.requestUsbCommand(command: .shutterOff) { error in
+        if error != nil {
+          os_log("Error: %@", type: .error, error?.localizedDescription ?? "")
+          return
+        }
+      }
+    }
+    self.showShutterOffToast.toggle()
+  }
+
+  func downloadMediaAll() {
+    os_log("Download Media All", type: .info)
+    self.getCreationTimestamp { creationTimestamp in
+      let creationDate = Date(timeIntervalSince1970: TimeInterval(creationTimestamp))
+      let dateFormatter = DateFormatter()
+      dateFormatter.dateFormat = "YYMMdd_hhmmss"
+      let creationDateString = dateFormatter.string(from: creationDate)
+      os_log("creationTimestamp: %@ from %@", type: .info, creationDateString, creationTimestamp.description)
+      for cameraConnectionInfo in self.getConnectedCameraList() {
+        os_log(
+          "Download media list: GoPro %@",
+          type: .info,
+          cameraConnectionInfo.camera.serialNumber
+        )
+        cameraConnectionInfo.camera.requestUsbMediaList { mediaEndPointList, _, error in
+          if error != nil {
+            os_log("Error: %@", type: .error, error?.localizedDescription ?? "")
+            return
+          }
+
+          for mediaEndPoint in mediaEndPointList ?? [] {
+            self.showDownloadMediaToast = true
+            cameraConnectionInfo.camera
+              .requestUsbMediaDownload(
+                mediaEndPoint: mediaEndPoint,
+                timestamp_path: creationDateString
+              ) { progress, error in
+                if error != nil {
+                  os_log("Error: %@", type: .error, error?.localizedDescription ?? "")
+                  return
+                }
+                if let progress {
+                  if progress > 99.9 {
+                    self.showDownloadMediaToast = false
+                  }
+                  self.downloadMediaUrl = "[GoPro " + cameraConnectionInfo.camera
+                    .serialNumber + "] " + (mediaEndPoint.split(separator: "/").last ?? "")
+                  self.downloadProgress = progress
+                }
+              }
+          }
+        }
+      }
+    }
+  }
+
+  func removeMediaAll() {
+    os_log("Remove Media All", type: .info)
+    for cameraConnectionInfo in self.getConnectedCameraList() {
+      os_log("Remove media list: GoPro %@", type: .info, cameraConnectionInfo.camera.serialNumber)
+      cameraConnectionInfo.camera.requestUsbMediaList { mediaEndPointList, _, error in
+        if error != nil {
+          os_log("Error: %@", type: .error, error?.localizedDescription ?? "")
+          return
+        }
+
+        for mediaUrl in mediaEndPointList ?? [] {
+          cameraConnectionInfo.camera
+            .requestUsbMediaRemove(mediaEndPoint: mediaUrl) { error in
+              if error != nil {
+                os_log("Error: %@", type: .error, error?.localizedDescription ?? "")
+                return
+              }
+            }
+        }
+      }
+    }
+    self.showRemoveMediaToast.toggle()
   }
 
   func applyPreset1() {
